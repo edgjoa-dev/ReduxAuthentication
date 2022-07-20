0d0613fdc8e9aae77660a476aedaf42e2493d9e2
Edgar Joaquin Flores <floresedgarjoaquin@gmail.com>
2022-07-20 14:30:21 -0500

    useForm implementado

--- src/auth/pages/LoginPages.jsx
+++ src/auth/pages/LoginPages.jsx
@@ -1,7 +1,51 @@
 import React from 'react'
+import { useForm } from '../../hooks/useForm'
+
+    const formData = {
+        email: 'edgarjoaquin@gmail.com',
+        password: '123456'
+    }
+
 
 export const LoginPages = () => {
+
+    const [ email, password, onInputChange ] = useForm(formData);
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log('handleSubmit')
+    }
+    const onGoogleSignIn = () => {
+        console.log('login with google')
+    }
+
     return (
-        <div>LoginPages</div>
+        <div>
+            <h1>LoginPages</h1>
+            <form onSubmit={handleSubmit}>
+                <input
+                type="text"
+                name="email"
+                value={email}
+                placeholder="email"
+                onChange={onInputChange}
+                />
+                <input
+                type="text"
+                name="password"
+                value={password}
+                placeholder="password"
+                onChange={onInputChange}
+                />
+                <button
+                    type="submit"
+                >Login</button>
+
+                <button
+                    type="submit"
+                    onClick={onGoogleSignIn}
+                >Login Google</button>
+            </form>
+        </div>
     )
 }
